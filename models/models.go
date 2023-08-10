ffcb82560eae4384c07df23be3b9713890cca871
SaeedReza <saeedreza.alipour@gmail.com>
2023-08-10 20:28:44 +0330

    feat: add user models

--- models/models.go
+++ models/models.go
@@ -8,3 +8,10 @@ type KeyValue struct {
 	Value     string    `gorm:"type:varchar(255);not null"`
 	CreatedAt time.Time `gorm:"type:datetime;autoCreateTime"`
 }
+
+type User struct {
+	ID       uint   `gorm:"primary_key"`
+	Username string `gorm:"type:varchar(255);unique;not null"`
+	Password string `gorm:"type:varchar(255);not null"`
+	Token    string `gorm:"not null"`
+}
