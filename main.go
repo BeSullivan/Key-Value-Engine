5bccb7e2eb26d2dc380f7cfe33e4d8223bdbd878
SaeedReza <saeedreza.alipour@gmail.com>
2023-08-10 21:52:52 +0330

    feat: add api documentation(Swagger)

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "Kiwi/server"
 
+//	@Title			Kiwi
+//	@version		1.0
+//	@description	KeyValue Engine Server
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host						localhost:8080
+// @BasePath					/
+// @query.collection.format	multi
+
 func main() {
 	server.StartServer()
 }
